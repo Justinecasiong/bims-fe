d1d3322b160aa198a55511f6069c3b2d919526ed
Julius Acidre <julius.acidre@intechsive.com>
2023-10-17 06:25:27 +0800

    minor bugs

--- src/views/Admin/OPT_Plus.vue
+++ src/views/Admin/OPT_Plus.vue
@@ -3,7 +3,7 @@
   <div class="page-header">
    <div class="row">
     <div class="col-lg-8 col-md-5">
-     <h1>Preschooler Nutritional Status</h1>
+     <h1>Operation Timbang Plus</h1>
     </div>
     <div class="col search">
      <div class="input -group">
